5aaeeeeb5172d956a039da2748295963ce964012
ShazidMahsrafi <shazid456@gmail.com>
2023-06-11 13:38:15 +0600

    update code

--- 112A Petya And Strings/112A.cpp
+++ 112A Petya And Strings/112A.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    string s1,s2;
+    char s1[1000],s2[1000];
     cin>>s1>>s2;
-    transform(s1.begin(),s1.end(),s1.begin(),::tolower);
-    transform(s2.begin(),s2.end(),s2.begin(),::tolower);
-    int c=s1.compare(s2);
+    strlwr(s1);
+    strlwr(s2);
+    int c=strcmp(s1,s2);
     cout<<c;
 }
