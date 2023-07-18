d107b6d024c0d1716da0222f707604a9acbb8e23
ShazidMahsrafi <shazid456@gmail.com>
2023-07-19 02:38:40 +0600

    update again

--- 208A Dubstep/208A.cpp
+++ 208A Dubstep/208A.cpp
@@ -1,46 +1,27 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-using ll = long long;
-
-
-void solve()
-{
-    string s, words;
-    cin >> s;
-    int i = 0;
-    while (i < s.size())
-    {
-        if (i + 2 < s.size() && s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
-        {
-            i += 3;
-        }
-        else
-        {
-            if (!words.empty())
-                words += ' ';
-            
-            while (i < s.size())
-            {
-                if (i + 2 < s.size() && s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
-                    break;
-                else
-                {
-                    words += s[i];
-                    i++;
-                }
-            }
-        }
-    }
-    cout << words << endl;
-}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int TC = 1;
-    //cin >> TC;
-    //cin.ignore();
-    while (TC--) solve();
+    string s;
+    int word = 0;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+        {
+            i += 2;
+            if (word)
+            {
+                word=0;
+                cout << " ";
+            }
+        }
+        else
+        {
+            word = 1;
+            cout << s[i];
+        }
+    }
+    return 0;
 }
