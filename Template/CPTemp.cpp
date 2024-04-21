daa39daeb4efd0282b9f7b1599ec9635c7bcde27
ShazidMahsrafi <shazid456@gmail.com>
2024-04-22 00:40:03 +0600

    update

--- Template/CPTemp.cpp
+++ Template/CPTemp.cpp
@@ -1,33 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define FAST_IO             (ios_base:: sync_with_stdio(false),cin.tie(NULL));
-#define ll                  long long
-#define ull                 unsigned long long
-#define endl                '\n'
-#define ff                  first
-#define ss                  second
-#define pb                  push_back
-#define all(x)              x.begin(), x.end()
-#define sz(x)               (int)(x).size()
-#define yes                 cout<<"YES"<<endl
-#define no                  cout<<"NO"<<endl
-#define neg1                cout<<-1<<endl
-#define PI                  3.141592653589793238
-#define MIN                 INT_MIN
-#define MAX                 INT_MAX
-#define INF                 LONG_LONG_MAX
-#define MOD                 1000000007
-#define LLM                 1000000000000000007
-
-ll factorial(ll n)                        { if(n==0) return 1; ll res = 1; for (ll i = 2; i <= n; i++) res = res * i; return res; }
-ll nPr(ll n, ll r)                        { return factorial(n) / factorial(n - r); }
-ll nCr(ll n, ll r)                        { return factorial(n) / (factorial(r) * factorial(n - r));}
-ll gcd(ll a, ll b)                        { if (b == 0) return a; return gcd(b, a % b); }
-ll lcm(ll a, ll b)                        { return (a * b) / gcd(a, b);}
-ull mypow(ull a, ull b,ull m=MOD)         { ull ans = 1; a%=m; while(b){ if (b&1) ans = (ans*a) % m; a = (a*a) % m; b >>= 1; } return ans; }
-bool isPrime(ll n)                        { if(n <= 1) return false; for(ll i = 2; i*i <= n; i++) if(n % i == 0) return false; return true; }
-
+#define ll                long long
+#define ull               unsigned long long
+#define endl              '\n'
+#define ff                first
+#define ss                second
+#define pb                push_back
+#define all(x)            x.begin(), x.end()
+#define sz(x)             (int)(x).size()
+#define yes               cout<<"YES"<<endl
+#define no                cout<<"NO"<<endl
+#define PI                3.141592653589793238
+#define MOD               1000000007
+#define FAST              (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+ull mypow(ull a,ull b,ull m=MOD) {ull ans=1; a%=m; while(b){if(b&1) ans=(ans*a)%m; a=(a*a)%m; b>>=1;} return ans;}
 #ifndef ONLINE_JUDGE
 #define dbg(x...) cerr << #x << " : "; _print(x)
 #else
@@ -51,9 +37,8 @@ void solve()
 
 signed main()
 {
-    FAST_IO;
+    FAST;
     int TC = 1;
     //cin >> TC;
-    while (TC--) 
-        solve();
+    while (TC--) solve();
 }
