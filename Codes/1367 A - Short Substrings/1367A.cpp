8e0b2666010e1cf6ce3cb1e28832d7ae5136f2ae
ShazidMahsrafi <shazid456@gmail.com>
2024-05-25 02:28:54 +0600

    add 1367A

---
+++ Codes/1367 A - Short Substrings/1367A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+ll pow(ll x,ll y,ll m=1e9+7){ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    string s;
+    cin>>s;
+    cout<<s[0];
+    for(int i=1; i<s.size()-1; i+=2)
+        cout<<s[i];
+    cout<<s.back()<<endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}
