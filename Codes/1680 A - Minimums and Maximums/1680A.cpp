232214b66b84b7dc3413cf2b1a0334743a49d7ec
ShazidMahsrafi <shazid456@gmail.com>
2024-08-21 09:13:22 +0600

    added 1106C, 1279A, 1301A, 1608A

---
+++ Codes/1680 A - Minimums and Maximums/1680A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include "Assets/debug.h"
+#endif
+#define  int  long long
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  ins  insert
+#define  pb  push_back
+#define  ppb  pop_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  rep(i,a,b)  for(int i=a; i<b; ++i)
+#define  rrep(i,a,b) for(int i=a; i>=b; --i)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll pow(ll x,ll y,ll m=1e9+7) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int l1,r1,l2,r2;
+    cin>>l1>>r1>>l2>>r2;
+    int ans=0;
+    rep(i,l1,r1+1)
+    {
+        if(i>=l2 && i<=r2)
+        {
+            ans=i;
+            break;
+        }
+    }
+    if(ans) cout<<ans<<endl;
+    else cout<<l1+l2<<endl;
+}
+
+signed main()
+{
+    FAST;
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+      // cout<<"Case "<<TC<<": ";
+      solve();
+    }
+}
