33957f1ef84c2872ca1a54f2d0532acbc1f9d807
ShazidMahsrafi <shazid456@gmail.com>
2024-08-31 12:22:22 +0600

    feat: Add solutions for Codeforces problems 2000 A, B, and 1440B

---
+++ Codes/1440 B - Sum of Medians/1440B.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include "Assets/debug.h"
+#endif
+#define  int  long long
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  ins  insert
+#define  pb  push_back
+#define  ppb  pop_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  rep(i,a,b)  for(int i=a; i<b; ++i)
+#define  rrep(i,a,b) for(int i=a; i>=b; --i)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll pow(ll x,ll y,ll m=1e9+7) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n,k,a;
+    vector<ll>v;
+    cin>>n>>k;
+    ll range = n*k,j = 0,div = 0,sum = 0;
+    div = n/2;
+
+    for(ll i=0; i<range; i++){
+        cin>>a;
+        v.pb(a);
+    }
+    for(ll i=range-1-div; i>=0; i-=div+1){
+        //cout<<v[i]<<" ";
+        sum += v[i];
+        dbg(v[i]);
+        j++;
+        if(j == k)break;
+    }
+    cout<<sum<<endl;
+}
+
+signed main()
+{
+    FAST;
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}
