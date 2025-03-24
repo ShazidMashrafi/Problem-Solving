d04285997ea836c3158a58071193df73fdf2f977
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-03-24 20:41:41 +0600

    add solutions for AtCoder problem B - Santa Claus 1, CodeChef problem SINS - The Deadly Sin, and Codeforces problems 1553A - Digits Sum and 1605A - A.M. Deviation

---
+++ Online Judges/CodeChef/Codes/SINS - The Deadly Sin/SINS.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  int  long long
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  ins  insert
+#define  pb  push_back
+#define  ppb  pop_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  rep(i,a,b)  for(int i=a; i<b; ++i)
+#define  rrep(i,a,b) for(int i=a; i>=b; --i)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll pow(ll x,ll y,ll m=1e9+7) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+    int ans = gcd(x, y);
+    cout << 2 * ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);    
+    // freopen("output.txt", "w", stdout);  
+
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}
