98a6d067d444950eaf3d0da94bd4f6cfef13e67c
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-03-26 18:12:03 +0600

    add solution for Codeforces problem 1015B - Obtaining the String

--- Online Judges/Codeforces/Codes/1015 B - Obtaining the String/1015B.cpp
+++ Online Judges/Codeforces/Codes/1015 B - Obtaining the String/1015B.cpp
@@ -19,7 +19,39 @@ ll power(ll x,ll y,ll m=1e9+7) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x
 
 void solve()
 {
-    
+    int n;
+    string s, t;
+    cin >> n >> s >> t;
+    vector<int> v;
+    for(int i = 0; i < n; ++i)
+    {
+        if(s[i] == t[i])    continue;
+        int p = -1;
+        for(int j = i + 1; j < n; ++j)
+        {
+            if(s[j] == t[i])
+            {
+                p = j;
+                break;
+            }
+        }
+        if(p == -1)
+        {
+            cout << -1 << endl;
+            return;
+        }
+        for(int j = p - 1; j >= i; --j)
+        {
+            swap(s[j], s[j + 1]);
+            v.pb(j);
+        }
+    }
+    cout << sz(v) << endl;
+    for (auto &&x : v)
+    {
+        cout << x + 1 << " ";
+    }
+    cout << endl;
 }
 
 signed main()
